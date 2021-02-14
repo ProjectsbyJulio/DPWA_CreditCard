34f405d3583cc85493131f9e6a65dfa5c9789eb4
Julio Eduardo Canizalez Salinas <juliocanizalez@outlook.com>
2021-02-13 21:58:38 -0600

    Agregar archivos de proyecto.

---
+++ dpwa_credit_card_exercise/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="dpwa_credit_card_exercise.Global" Language="C#" %>
---
+++ dpwa_credit_card_exercise/Global.asax.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace dpwa_credit_card_exercise
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+        }
+    }
+}
