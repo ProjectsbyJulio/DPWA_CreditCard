0ea29052cdc1d66f0f7e416d45dcc1f4fe9b92e3
Julio Eduardo Canizalez Salinas <juliocanizalez@outlook.com>
2021-02-18 22:40:37 -0600

    :lipstick: Add view

---
+++ dpwa_credit_card_exercise/Views/Home.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Home.Master" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="dpwa_credit_card_exercise.Home1" %>
+
+<%@ Register Src="~/UserControl/InputTarjeta.ascx" TagName="ValidarTarjeta" TagPrefix="ucl" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <ucl:ValidarTarjeta ID="ValidarTarjeta1" runat="server" />
+
+</asp:Content>
---
+++ dpwa_credit_card_exercise/Views/Home.aspx.designer.cs
@@ -0,0 +1,26 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace dpwa_credit_card_exercise
+{
+
+
+    public partial class Home1
+    {
+
+        /// <summary>
+        /// Control ValidarTarjeta1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::dpwa_credit_card_exercise.InputText ValidarTarjeta1;
+    }
+}
